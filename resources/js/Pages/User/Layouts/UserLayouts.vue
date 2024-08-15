5e46d0eee5a06a222a0a80ba6364f1a6f5b05617
Zakariae Azn <azarkanzakariae@gmail.com>
2024-08-15 15:12:51 +0100

    Project Files

--- resources/js/Pages/User/Layouts/UserLayouts.vue
+++ resources/js/Pages/User/Layouts/UserLayouts.vue
@@ -3,7 +3,6 @@ import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
 import Header from './Header.vue';
 import Footer from './Footer.vue';
-import Hero from './Hero.vue';
 
 // initialize components based on data attribute selectors
 onMounted(() => {
